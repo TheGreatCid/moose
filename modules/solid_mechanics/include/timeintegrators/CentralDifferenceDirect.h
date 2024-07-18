cd6dc21dfecdb32cfc36d5accdf2f55075a7f601
TheGreatCid <davidt.realmadrid@gmail.com>
2024-07-18 13:35:45 -0600

    added general central difference method to solid mechanics #27883

---
+++ modules/solid_mechanics/include/timeintegrators/CentralDifferenceDirect.h
@@ -0,0 +1,56 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ExplicitTimeIntegrator.h"
+#include "libmesh/sparse_matrix.h"
+
+class MassMatrixHelper;
+/**
+ * Implements a truly explicit (no nonlinear solve) first-order, forward Euler
+ * time integration scheme.
+ */
+class CentralDifferenceDirect : public ExplicitTimeIntegrator
+{
+public:
+  static InputParameters validParams();
+
+  CentralDifferenceDirect(const InputParameters & parameters);
+
+  virtual int order() override { return 1; }
+  virtual void computeTimeDerivatives() override;
+
+  virtual void solve() override;
+  virtual void postResidual(NumericVector<Number> & residual) override;
+
+  virtual bool performExplicitSolve(SparseMatrix<Number> & mass_matrix) override;
+
+  // Applying pre-set nodal BCs
+  void setPresetBCs();
+
+  void computeADTimeDerivatives(ADReal & ad_u_dot,
+                                const dof_id_type & dof,
+                                ADReal & ad_u_dotdot) const override
+  {
+    ad_u_dotdot.value();
+    ad_u_dot.value();
+    std::cout << dof << std::endl;
+    return mooseError("NOT SUPPORTED");
+  }
+
+protected:
+  /**
+   * Helper function that actually does the math for computing the time derivative
+   */
+
+  const bool & _constant_mass;
+
+  const TagName & _mass_matrix;
+};
